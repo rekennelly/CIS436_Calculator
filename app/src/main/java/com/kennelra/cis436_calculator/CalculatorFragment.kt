3fc0def4568c3666f0dc31ce9b10eb6445559e37
Rachel Kennelly <rekennelly@gmail.com>
2021-04-27 16:02:56 -0400

    app no longer crashes on rotation

--- app/src/main/java/com/kennelra/cis436_calculator/CalculatorFragment.kt
+++ app/src/main/java/com/kennelra/cis436_calculator/CalculatorFragment.kt
@@ -3,13 +3,11 @@ package com.kennelra.cis436_calculator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.widget.Button
+import android.widget.TextView
 
 /**
  * A simple [Fragment] subclass.
@@ -17,43 +15,35 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class CalculatorFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var calcInput : TextView
+
+    val ERROR_MSG : String = "ERROR"
+
+    // Track if last key pressed is a number
+    var lastBtnPressIsDigit : Boolean = false
+    // Track if last key pressed is the decimal point (don't allow 2 in a row)
+    var lastBtnPressIsDecimal : Boolean = false
+    // Track if calculator is in Error Mode
+    var errorState : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calculator, container, false)
+        var view = inflater.inflate(R.layout.fragment_calculator, container, false)
+
+        calcInput = view.findViewById(R.id.screenTextView)
+
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CalculatorFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CalculatorFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = CalculatorFragment()
     }
+
 }
--- app/src/main/java/com/kennelra/cis436_calculator/MainActivity.kt
+++ app/src/main/java/com/kennelra/cis436_calculator/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.kennelra.cis436_calculator
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Attach the frame layout to the
     }
 }
