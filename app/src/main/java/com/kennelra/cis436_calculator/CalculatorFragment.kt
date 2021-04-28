860432a028b525e62a133e6adc263eb01b139def
Rachel Kennelly <rekennelly@gmail.com>
2021-04-27 21:39:06 -0400

    most buttons functioning

--- app/src/main/java/com/kennelra/cis436_calculator/CalculatorFragment.kt
+++ app/src/main/java/com/kennelra/cis436_calculator/CalculatorFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import kotlin.math.min
 
 /**
  * A simple [Fragment] subclass.
@@ -35,39 +36,132 @@ class CalculatorFragment : Fragment() {
     is pressed. (All buttons styled with numberbtn.xml */
     fun onNumberBtnClick(v : View?) {
         with (v as Button) {
-            // Get digit from button
-            val number: Int = v.text.toString().toInt()
+            if (!errorState) {
+                // Get digit from button
+                val number: Int = v.text.toString().toInt()
 
-            // Append that digit to the calculator screen
-            calcScreen.append(number.toString())
+                // Append that digit to the calculator screen
+                calcScreen.append(number.toString())
 
-            // Set last button pressed is digit to True
-            lastBtnPressIsDigit = true
+                // Set last button pressed is digit to True
+                lastBtnPressIsDigit = true
+                // Set last button pressed is decimal to false
+                lastBtnPressIsDecimal = false
+            }
         }
     }
 
     /* Helper function to call when an operator button
-    is pressed. (All buttons styled with operatorbtn.xml */
-    fun operateOperatorButton(v: View) {
+    is pressed. (All buttons styled with operatorbtn.xml EXCEPT square root) */
+    fun onOperatorBtnClick(v: View) {
         with (v as Button) {
-            if (lastBtnPressIsDigit) {
-                // Get operator from button
-                val operator : String = v.text.toString()
+            if (!errorState) {
+                if (lastBtnPressIsDigit) {
+                    // Get operator from button
+                    val operator: String = v.text.toString()
 
-                // Append to screen
-                calcScreen.append(operator)
+                    // Append to screen
+                    calcScreen.append(operator)
 
-                // Set last button pressed is digit to False
-                lastBtnPressIsDigit = false
-            } else {
-                // Show error message
-                calcScreen.text = ERROR_MSG
+                    // Set last button pressed is digit to False
+                    lastBtnPressIsDigit = false
+                    // Set last button pressed is decimal to false
+                    lastBtnPressIsDecimal = false
+                } else {
+                    // Show error message
+                    calcScreen.text = ERROR_MSG
+                    errorState = true
+                }
             }
-
         }
     }
 
-    /* Helper function to call when a setting btn is pressed (except equals) */
+    /* Helper function to call when the square root button is pressed. */
+    fun onSqrtBtnClick(v: View) {
+        with (v as Button) {
+            if (!errorState) {
+                // Get symbol from button
+                val sqrt: String = v.text.toString()
+
+                // Append the sqrt symbol to the calculator screen
+                calcScreen.append(sqrt.toString())
+
+                // Set last button pressed is digit to false
+                lastBtnPressIsDigit = false
+                // Set last button pressed is decimal to false
+                lastBtnPressIsDecimal = false
+            }
+        }
+    }
+
+    /* Helper function to call when the clear button is pressed. */
+    fun onClearBtnClick(v: View) {
+        with (v as Button) {
+            // Clear the calculator screen
+            calcScreen.text = null
+
+            // Set last button pressed is digit to false
+            lastBtnPressIsDigit = false
+            // Set last button pressed is decimal to false
+            lastBtnPressIsDecimal = false
+
+            // Clear error state
+            errorState = false
+        }
+    }
+
+    /* Helper function to call when the decimal button is pressed. */
+    fun onDecimalBtnClick(v: View) {
+        with (v as Button) {
+            if (!errorState) {
+                // Get symbol from button
+                val decimal: String = v.text.toString()
+
+                if (lastBtnPressIsDigit) {
+                    // Append the decimal point to the calculator screen
+                    calcScreen.append(decimal)
+                } else if (!lastBtnPressIsDecimal){
+                    // Append a zero followed by a decimal point to the calculator screen
+                    calcScreen.append("0")
+                    calcScreen.append(decimal)
+                } else {
+                    // Throw error on calc screen
+                    calcScreen.text = ERROR_MSG
+                    errorState = true
+                }
+
+                // Set last button pressed is digit to false
+                lastBtnPressIsDigit = false
+                // Set last button pressed is decimal to true
+                lastBtnPressIsDecimal = true
+            }
+        }
+    }
+
+    /* Helper function to call when the plus minus button is pressed. */
+    // if not error state
+            // if last char of calc screen is negative
+                // delete negative symbol
+            // if last char of calc screen is number or decimal
+                // put negative sign before number after last operator
+            // else
+                // put negative sign
+        // else
+            // throw error
+
+
+    // EQUALS BUTTON
+    /* Order of Operations (left to right):
+    Square Roots
+    Multiply/Divide/Mod
+    Add / Subtract
+    */
+        // [45, +, 5, *, 3, %, 4]
+        // find first symbol with OoO
+        // [45, +, 15, %, 4]
+        // execute the operation with the values in indices i-1 and i+1
+        // [
+
 
     override fun onCreateView (
         inflater: LayoutInflater, container: ViewGroup?,
@@ -132,6 +226,34 @@ class CalculatorFragment : Fragment() {
         }
         zeroBtn.setOnClickListener{view ->
             onNumberBtnClick(view)
+        }
+
+        // Set operator buttons
+        addBtn.setOnClickListener{view ->
+            onOperatorBtnClick(view)
+        }
+        minusBtn.setOnClickListener{view ->
+            onOperatorBtnClick(view)
+        }
+        multiplyBtn.setOnClickListener{view ->
+            onOperatorBtnClick(view)
+        }
+        divideBtn.setOnClickListener{view ->
+            onOperatorBtnClick(view)
+        }
+        modBtn.setOnClickListener{view ->
+            onOperatorBtnClick(view)
+        }
+        sqrtBtn.setOnClickListener{view ->
+            onSqrtBtnClick(view)
+        }
+
+        // Set settings buttons
+        clearBtn.setOnClickListener{view ->
+            onClearBtnClick(view)
+        }
+        decimalBtn.setOnClickListener{view ->
+            onDecimalBtnClick(view)
         }
 
         return view
